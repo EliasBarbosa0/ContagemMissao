6326955b24f91f4adcba07dd9a226640ecdad69e
Elias Barbosa <contato@eliasbarbosa.com>
2024-08-16 11:20:42 -0300

    Simplificação e redução no número de caracteres.

--- TextoTwitter.py
+++ TextoTwitter.py
@@ -12,9 +12,9 @@ def corpo():
         f'⌚️ Hora da atualização: {dh(valor[7])}\n'
         f'🚀 Validados: {f(valor[0])} ({ fd(valor[0] / valor[1] * 100 )}%)\n'
         f'🎯 Necessários: {f(valor[1])}\n'
-        f'📅 Projeção da conclusão: {dt(valor[6])}\n'
-        f'📈 Média dia (sete dias): {f(valor[9])}\n'
-        f'🇧🇷 Estados que atingiram o mínimo: {valor[5]} de {valor[4]}\n\n'
+        f'🗓️ Projeção da conclusão: {dt(valor[6])}\n'
+        f'📈 Média diária: {f(valor[9])}\n'
+        f'✅ Estados que atingiram o mínimo: {valor[5]}\n\n'
         f'🌐 Relatório completo: https://bit.ly/4fjX8Zy\n\n'
         f'@PartidoMissao\n'
     )
